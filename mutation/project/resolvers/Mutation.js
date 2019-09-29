d2126913066edbcdc8b987015b1316205d498e8a
Victor Mattar <suporte@trustit.com.br>
2019-09-29 20:59:46 -0300

    inputs implementation

--- mutation/project/resolvers/Mutation.js
+++ mutation/project/resolvers/Mutation.js
@@ -1,10 +1,22 @@
 const { usuarios, proximoId } = require('../data/db')
 
+function indiceUsuario(filtro) {
+    if(!filtro) return -1
+    const { id, email } = filtro
+
+    if(id)
+        return usuarios.findIndex((f) => f.id === id)
+    else if(email)
+        return usuarios.findIndex((f) => f.email === email)
+
+    return -1
+}
+
 module.exports = {
     // { nome, email, idade }
-    novoUsuario(_, args ){
+    novoUsuario(_, { dados } ){
         const emailExistente = usuarios
-            .some(u => u.email === args.email)
+            .some(u => u.email === dados.email)
 
         if(emailExistente)
             throw new Error('E-mail cadastrado')
@@ -12,7 +24,7 @@ module.exports = {
 
         const novo = {
             id: proximoId(),
-            ...args,
+            ...dados,
             perfil_id: 1,
             status: 'ATIVO',
         }
@@ -21,22 +33,20 @@ module.exports = {
         return novo
     },
 
-    excluirUsuario(_, { id }){
-        const i = usuarios.findIndex(f => f.id === id)
+    excluirUsuario(_, { filtro }){
+        const i = indiceUsuario(filtro)
         if(i < 0) return null
         let excluido = usuarios.splice(i, 1)
         return excluido ? excluido[0] : null
     },
 
-    alterarUsuario(_, args) {
-        const i = usuarios.findIndex(f => f.id === args.id)
+    alterarUsuario(_, { filtro, dados } ) {
+        const i = indiceUsuario(filtro)
         if(i < 0) return null
-
         const usuario = {
             ...usuarios[i],
-            ...args
+            ...dados
         }
-
         usuarios.splice(i, 1, usuario)
         return usuarios[i]
     }

0c68a2a8f79c6c2216ad9f970cec1fad6a5b7fc4
Victor Mattar <suporte@trustit.com.br>
2019-09-29 16:33:02 -0300

    crud operations

--- mutation/project/resolvers/Mutation.js
+++ mutation/project/resolvers/Mutation.js
@@ -1,18 +1,43 @@
 const { usuarios, proximoId } = require('../data/db')
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade }){
+    // { nome, email, idade }
+    novoUsuario(_, args ){
+        const emailExistente = usuarios
+            .some(u => u.email === args.email)
+
+        if(emailExistente)
+            throw new Error('E-mail cadastrado')
+
+
         const novo = {
             id: proximoId(),
-            nome,
-            email,
-            idade,
+            ...args,
             perfil_id: 1,
             status: 'ATIVO',
         }
 
         usuarios.push(novo)
         return novo
-    }
+    },
 
+    excluirUsuario(_, { id }){
+        const i = usuarios.findIndex(f => f.id === id)
+        if(i < 0) return null
+        let excluido = usuarios.splice(i, 1)
+        return excluido ? excluido[0] : null
+    },
+
+    alterarUsuario(_, args) {
+        const i = usuarios.findIndex(f => f.id === args.id)
+        if(i < 0) return null
+
+        const usuario = {
+            ...usuarios[i],
+            ...args
+        }
+
+        usuarios.splice(i, 1, usuario)
+        return usuarios[i]
+    }
 }

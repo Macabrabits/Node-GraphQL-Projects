712b6a060eba541943c35ba685dddb71b4fbf1f8
Victor Mattar <suporte@trustit.com.br>
2019-09-29 22:48:09 -0300

    File organization + (perfil)CRUD

--- mutation/project/resolvers/Query.js
+++ mutation/project/resolvers/Query.js
@@ -1,28 +1,13 @@
 const { usuarios, perfis } = require('../data/db')
 
-function indiceUsuario(filtro) {
-    if(!filtro) return -1
-    const { id, email } = filtro
-
-    if(id)
-        return usuarios.findIndex((f) => f.id === id)
-    else if(email)
-        return usuarios.findIndex((f) => f.email === email)
-
-    return -1
-}
-
-
 module.exports = {
     usuarios() {
         return usuarios
     },
-    usuario(_, { filtro }) {
-        // const sels = usuarios
-        //     .filter(u => u.id === id)
-        const i = indiceUsuario(filtro)
-        if(i < 0)return null
-        return usuarios[i]
+    usuario(_, { id }) {
+        const sels = usuarios
+            .filter(u => u.id === id)
+        return sels ? sels[0] : null
     },
     perfis() {
         return perfis
@@ -30,6 +15,6 @@ module.exports = {
     perfil(_, { id }) {
         const sels = perfis
             .filter(p => p.id === id)
-        return sels ? sels[0] : null 
+        return sels ? sels[0] : null
     }
 }
--- mutation/project/resolvers/Usuario.js
+++ mutation/project/resolvers/Usuario.js
@@ -1,3 +1,4 @@
+
 const { perfis } = require('../data/db')
 
 module.exports = {
